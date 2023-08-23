9d7c59180d04b710946ac09f65c724b3f95f83bb
Rayza Mahendra <rayzaganteng@gmail.com>
2023-08-23 13:50:04 +0700

    increase temp

--- module/message/agent/agent.py
+++ module/message/agent/agent.py
@@ -60,11 +60,11 @@ class SongAgent:
     """
 
     mem_model: BaseLanguageModel = ChatOpenAI(
-        model_name="gpt-3.5-turbo-0613", temperature=0)
+        model_name="gpt-3.5-turbo-0613", temperature=0.5)
     style_model: BaseLanguageModel = ChatOpenAI(
-        model_name="gpt-3.5-turbo-0613", temperature=0.6)
+        model_name="gpt-3.5-turbo-0613", temperature=1)
     chat_model: BaseLanguageModel = ChatOpenAI(
-        model_name="gpt-3.5-turbo-16k", temperature=0.8)
+        model_name="gpt-3.5-turbo-16k", temperature=1)
 
     def __init__(self, complex_agent=True):
         """
