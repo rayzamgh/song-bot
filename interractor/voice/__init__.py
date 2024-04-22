222aed22a73f2ca8d47e2e8c70074d0be0319f23
rayzamgh <rayzaganteng@gmail.com>
2024-04-23 00:04:19 +0700

    silero removed

--- interractor/voice/__init__.py
+++ interractor/voice/__init__.py
@@ -1,4 +1,4 @@
 from .voice_stt import SpeechToTextInterractor
 from .streamsink import StreamSink
-from .silero import SilenceDetector
+# from .silero import SilenceDetector
 from .voice_tts import TextToSpeechInterractor
